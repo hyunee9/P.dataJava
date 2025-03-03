f6bb6e613255bfa430c46befbc70198733179360
HyunHee <h2koo2004@gmail.com>
2025-03-03 22:21:20 +0900

    Day05 - 상속

---
+++ src/oop/inherit/Main.java
@@ -0,0 +1,14 @@
+package oop.inherit;
+
+import oop.static_.field.Count;
+
+public class Main {
+    public static void main(String
+                                    [] args) {
+
+        Warrior w1 = new Warrior("전사짱");
+        w1.showStatus();
+    }
+
+
+}
